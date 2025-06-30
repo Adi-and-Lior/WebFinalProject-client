0402d0a80e4cece94af638af678c5ce7650ae673
LiorBoaron <lior.boaron8@gmail.com>
2025-06-30 14:03:47 +0300

    fix: enabling the my reports button

--- js/homePageCitizen.js
+++ js/homePageCitizen.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const newReportButton = document.getElementById('newReportButton');
+    const myReportsButton = document.getElementById('myReportsButton');
     if (newReportButton) {
         newReportButton.addEventListener('click', () => {
             window.location.href = '../html/newReportPage.html'; 
         });
     } else {
         console.warn("Element with ID 'newReportButton' not found. Cannot attach click listener.");
+    }
+
+    if (myReportsButton) {
+        myReportsButton.addEventListener('click', () => {
+            window.location.href = '../html/myReportsPage.html'; 
+        });
+    } else {
+        console.warn("Element with ID 'myReportsButton' not found. Cannot attach click listener.");
     }
 });
